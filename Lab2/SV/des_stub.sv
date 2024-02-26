3e78c2685e1196c088697d9882264acb46036b98
CarsonKring <ckring@okstate.edu>
2024-02-26 13:43:48 -0600

    Hopefully finished

--- Lab2/SV/des_stub.sv
+++ Lab2/SV/des_stub.sv
@@ -181,6 +181,39 @@ module SF (inp_block, out_block);
    input logic [31:0] inp_block;
    output logic [31:0] out_block;
 
+   assign out_block[31] = inp_block[32-16];
+   assign out_block[30] = inp_block[32-7];
+   assign out_block[29] = inp_block[32-20];
+   assign out_block[28] = inp_block[32-21];
+   assign out_block[27] = inp_block[32-29];
+   assign out_block[26] = inp_block[32-12];
+   assign out_block[25] = inp_block[32-28];
+   assign out_block[24] = inp_block[32-17];
+   assign out_block[23] = inp_block[32-1];
+   assign out_block[22] = inp_block[32-15];
+   assign out_block[21] = inp_block[32-23];
+   assign out_block[20] = inp_block[32-26];
+   assign out_block[19] = inp_block[32-5];
+   assign out_block[18] = inp_block[32-18];
+   assign out_block[17] = inp_block[32-31];
+   assign out_block[16] = inp_block[32-10];
+   assign out_block[15] = inp_block[32-2];
+   assign out_block[14] = inp_block[32-8];
+   assign out_block[13] = inp_block[32-24];
+   assign out_block[12] = inp_block[32-14];
+   assign out_block[11] = inp_block[32-32];
+   assign out_block[10] = inp_block[32-27];
+   assign out_block[9] = inp_block[32-3];
+   assign out_block[8] = inp_block[32-9];
+   assign out_block[7] = inp_block[32-19];
+   assign out_block[6] = inp_block[32-13];
+   assign out_block[5] = inp_block[32-30];
+   assign out_block[4] = inp_block[32-6];
+   assign out_block[3] = inp_block[32-22];
+   assign out_block[2] = inp_block[32-11];
+   assign out_block[1] = inp_block[32-4];
+   assign out_block[0] = inp_block[32-25];
+
 endmodule // SF
 
 // Expansion Function
@@ -188,6 +221,55 @@ module EF (inp_block, out_block);
 
    input logic [31:0] inp_block;
    output logic [47:0] out_block;
+
+   assign out_block[47]=inp_block[32-32];
+   assign out_block[46]=inp_block[32-1];
+   assign out_block[45]=inp_block[32-2];
+   assign out_block[44]=inp_block[32-3];
+   assign out_block[43]=inp_block[32-4];
+   assign out_block[42]=inp_block[32-5];
+   assign out_block[41]=inp_block[32-4];
+   assign out_block[40]=inp_block[32-5];
+   assign out_block[39]=inp_block[32-6];
+   assign out_block[38]=inp_block[32-7];
+   assign out_block[37]=inp_block[32-8];
+   assign out_block[36]=inp_block[32-9];
+   assign out_block[35]=inp_block[32-8];
+   assign out_block[34]=inp_block[32-9];
+   assign out_block[33]=inp_block[32-10];
+   assign out_block[32]=inp_block[32-11];
+   assign out_block[31]=inp_block[32-12];
+   assign out_block[30]=inp_block[32-13];
+   assign out_block[29]=inp_block[32-12];
+   assign out_block[28]=inp_block[32-13];
+   assign out_block[27]=inp_block[32-14];
+   assign out_block[26]=inp_block[32-15];
+   assign out_block[25]=inp_block[32-16];
+   assign out_block[24]=inp_block[32-17];
+   assign out_block[23]=inp_block[32-16];
+   assign out_block[22]=inp_block[32-17];
+   assign out_block[21]=inp_block[32-18];
+   assign out_block[20]=inp_block[32-19];
+   assign out_block[19]=inp_block[32-20];
+   assign out_block[18]=inp_block[32-21];
+   assign out_block[17]=inp_block[32-20];
+   assign out_block[16]=inp_block[32-21];
+   assign out_block[15]=inp_block[32-22];
+   assign out_block[14]=inp_block[32-23];
+   assign out_block[13]=inp_block[32-24];
+   assign out_block[12]=inp_block[32-25];
+   assign out_block[11]=inp_block[32-24];
+   assign out_block[10]=inp_block[32-25];
+   assign out_block[9]=inp_block[32-26];
+   assign out_block[8]=inp_block[32-27];
+   assign out_block[7]=inp_block[32-28];
+   assign out_block[6]=inp_block[32-29];
+   assign out_block[5]=inp_block[32-28];
+   assign out_block[4]=inp_block[32-29];
+   assign out_block[3]=inp_block[32-30];
+   assign out_block[2]=inp_block[32-31];
+   assign out_block[1]=inp_block[32-32];
+   assign out_block[0]=inp_block[32-1];
 
 endmodule // EF
 
@@ -197,6 +279,33 @@ module feistel (inp_block, subkey, out_block);
    input logic [47:0]  subkey;
    output logic [31:0] out_block;
 
+   logic [47:0] expanded;
+   logic [47:0] xored;
+   logic [5:0] s1,s2,s3,s4,s5,s6,s7,s8;
+   logic [5:0] so1,so2,so3,so4,so5,so6,so7,so8;
+
+   EF e(inp_block,expanded);
+   assign xored = expanded ^ subkey;
+	assign s1 = xored[47:42];
+	assign s2 = xored[41:36];
+	assign s3 = xored[35:30];
+	assign s4 = xored[29:24];
+	assign s5 = xored[23:18];
+	assign s6 = xored[17:12];
+	assign s7 = xored[11:6];
+	assign s8 = xored[5:0];
+
+	S1_Box sbox1(s1,so1);
+	S2_Box sbox2(s2,so2);
+	S3_Box sbox3(s3,so3);
+	S4_Box sbox4(s4,so4);
+	S5_Box sbox5(s5,so5);
+	S6_Box sbox6(s6,so6);
+	S7_Box sbox7(s7,so7);
+	S8_Box sbox8(s8,so8);
+
+	SF straight({so1,so2,so3,so4,so5,so6,so7,so8},out_block);
+
 endmodule // Feistel
 
 // DES block round
@@ -205,6 +314,20 @@ module round (inp_block, subkey, out_block);
    input logic [63:0]  inp_block;
    input logic [47:0]  subkey;
    output logic [63:0] out_block;
+
+   logic [31:0] inp_left,inp_right;
+	logic [31:0] feistel_out;
+	logic [31:0] out_right;
+
+	assign inp_left = inp_block[63:32];
+   assign inp_right = inp_block[31:0];
+
+   //run feistel block
+	feistel f(inp_right, subkey, feistel_out);
+	assign out_right = inp_left^feistel_out;
+
+   assign out_block[63:32]=inp_right;
+   assign out_block[31:0]=out_right;
 
 endmodule // round1
 
@@ -999,41 +1122,57 @@ module DES (input logic [63:0] key, input logic [63:0] plaintext,
 		    SubKey13, SubKey14, SubKey15, SubKey16);
    // encrypt (encrypt=1) or decrypt (encrypt=0) 
 
+	logic [63:0] rout1,rout2,rout3,rout4,rout5,rout6,rout7,rout8,rout9,rout10,rout11,rout12,rout13,rout14,rout15,rout16;
    // Initial Permutation (IP)
    IP b1 (plaintext, ip_out);
    // round 1
+   round r1 (ip_out, SubKey1, rout1);
    
    // round 2
+   round r2 (rout1, SubKey2, rout2);
    
    // round 3
+   round r3 (rout2, SubKey3, rout3);
    
    // round 4
+   round r4 (rout3, SubKey4, rout4);
    
    // round 5
+   round r5 (rout4, SubKey5, rout5);
    
    // round 6
+   round r6 (rout5, SubKey6, rout6);
    
    // round 7
+   round r7 (rout6, SubKey7, rout7);
    
    // round 8
+   round r8 (rout7, SubKey8, rout8);
    
    // round 9
+   round r9 (rout8, SubKey9, rout9);
    
    // round 10
+   round r10 (rout9, SubKey10, rout10);
    
    // round 11
+   round r11 (rout10, SubKey11, rout11);
    
    // round 12
+   round r12 (rout11, SubKey12, rout12);
    
    // round 13
+   round r13 (rout12, SubKey13, rout13);
    
    // round 14
+   round r14 (rout13, SubKey14, rout14);
    
    // round 15
+   round r15 (rout14, SubKey15, rout15);
    
    // round 16
+   round r16 (rout15, SubKey16, rout16);
 
    // Final Permutation (IP^{-1}) (swap output of round16)
-   FP FP({r16_out[31:0], r16_out[63:32]}, ciphertext);
-   //tes test test again
+   FP FP({rout16[31:0], rout16[63:32]}, ciphertext);
 endmodule // DES
