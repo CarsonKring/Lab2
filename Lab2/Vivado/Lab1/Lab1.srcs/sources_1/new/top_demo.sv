66f5ccd5cd517bc7997ffc2f4bfa0b1b82d03b07
Kring <ckring@okstate.edu>
2024-03-04 13:36:11 -0600

    Mostly Done

--- Lab2/Vivado/Lab1/Lab1.srcs/sources_1/new/top_demo.sv
+++ Lab2/Vivado/Lab1/Lab1.srcs/sources_1/new/top_demo.sv
@@ -43,17 +43,40 @@ module top_demo
   logic [16:0] CURRENT_COUNT;
   logic [16:0] NEXT_COUNT;
   logic        smol_clk;
-  
+  logic [63:0] key1;
+  logic [63:0] plaintext1, ciphertext1;
+  assign key1 = 64'h0123456789ABCDEF;
+  assign plaintext1 = 64'hAAAABBBBCCCCDDDD;
   // Place TicTacToe instantiation here
-  
+  DES dut(key1,plaintext1,sw[7],ciphertext1);
+  logic[1:0] keyinp;
+  assign keyinp = {sw[1],sw[0]};
+  logic[15:0] output1;
+  logic[63:0] inptext;
+  always @(inptext,ciphertext1,sw[3:2],plaintext1,key1) begin
+  case(sw[3:2])
+    2'b00: inptext = plaintext1;
+    2'b01: inptext = key1;
+    2'b10: inptext = ciphertext1;
+    2'b11: inptext = ciphertext1;
+  endcase
+  end
+  always @(output1,inptext,sw[1:0]) begin
+  case(sw[1:0])
+    2'b00: output1 = inptext[63:48];
+    2'b01: output1 = inptext[47:32];
+    2'b10: output1 = inptext[31:16];
+    2'b11: output1 = inptext[15:0];
+  endcase
+  end
   // 7-segment display
   segment_driver driver(
   .clk(smol_clk),
   .rst(btn[3]),
-  .digit0(sw[3:0]),
-  .digit1(4'b0111),
-  .digit2(sw[7:4]),
-  .digit3(4'b1111),
+  .digit3(output1[15:12]),
+  .digit2(output1[11:8]),
+  .digit1(output1[7:4]),
+  .digit0(output1[3:0]),
   .decimals({1'b0, btn[2:0]}),
   .segment_cathodes({sseg_dp, sseg_cg, sseg_cf, sseg_ce, sseg_cd, sseg_cc, sseg_cb, sseg_ca}),
   .digit_anodes(sseg_an)
